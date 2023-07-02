ef8098c4797922129796c3ab0c94f0a3e43c56ca
iptime-fox <dudndi826@gmail.com>
2023-07-02 10:04:08 +0900

    #4.0 REACT ROUTER V6

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,9 +1,41 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
 
-function Circle({ bgColor }) {
-  return <Container />;
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 3px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+}
+
+function Circle({ bgColor, borderColor }: CircleProps) {
+  const [value, setValue] = useState(0);
+
+  return (
+    <Container
+      bgColor={bgColor}
+      borderColor={borderColor ?? 'white'}></Container>
+  );
 }
 
 export default Circle;
+
+interface PlayerShape {
+  name: string;
+  age: string;
+}
+const sayHello = (playerObj: PlayerShape) =>
+  `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
+
+sayHello({ name: 'nara', age: '12' });
